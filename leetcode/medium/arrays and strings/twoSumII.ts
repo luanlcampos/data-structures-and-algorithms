036df04c902ae44519c17ddbf4d92a636a8867ea
luanlcampos <luanlcampos15@gmail.com>
2023-02-14 10:18:59 -0800

    add two sum II

---
+++ leetcode/medium/arrays and strings/twoSumII.ts
@@ -0,0 +1,18 @@
+function twoSum(numbers: number[], target: number): number[] {
+  let pLeft = 0,
+    pRight = numbers.length - 1;
+
+  // two pointers solution
+  while (pLeft < pRight) {
+    let sum = numbers[pLeft] + numbers[pRight];
+    if (sum === target) {
+      return [pLeft + 1, pRight + 1];
+    } else if (sum > target) {
+      pRight--;
+    } else if (sum < target) {
+      pLeft++;
+    }
+  }
+
+  return [];
+}
