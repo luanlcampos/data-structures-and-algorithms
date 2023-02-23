71200dca12ad81004bb56432f7292ee201a4bc15
luanlcampos <luanlcampos15@gmail.com>
2023-02-23 10:49:22 -0800

    remove avoidable ifs

--- leetcode/medium/arrays and strings/groupAnagrams.ts
+++ leetcode/medium/arrays and strings/groupAnagrams.ts
@@ -1,20 +1,19 @@
 function groupAnagrams(strs: string[]): string[][] {
+  // we are going to use a map<sortedString, string[]>
+  // loop through the array of strings:
+  // 1. sort each string
+  // 2. check if it's already in the map
+  // 3. if so, push the unsorted string to the array of strings
+  // 4. if not, add the key to the hashmap
+
   const map: Map<string, string[]> = new Map();
 
   for (let str of strs) {
-    // sort the string
     let sortedString = str.split("").sort().join("");
-
-    // check if the sorted string is a key in the map, if so,
-    // add the unsorted string to the key valu
-    if (map.has(sortedString)) {
-      map.set(sortedString, [...map.get(sortedString), str]);
-    }
-    // if not, add the key to the map
-    else {
-      map.set(sortedString, [str]);
-    }
+    map.set(sortedString, [...(map.get(sortedString) || []), str]);
   }
 
   return Array.from(map.values());
 }
+
+console.log(groupAnagrams(["eat", "ate", "dog", "pog"]));
